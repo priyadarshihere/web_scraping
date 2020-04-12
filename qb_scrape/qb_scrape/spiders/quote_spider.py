3b7db64c64aaf0fb8260f61678b7b87f6bcf3f75
JYOTSNA PRIYADARSHI <jpriyadarshi3@gmail.com>
2020-04-12 10:25:58 +0530

    first commit

---
+++ qb_scrape/qb_scrape/spiders/quote_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+from ..items import QbScrapeItem
+
+
+class QuoteSpider(scrapy.Spider):
+	name='quotes'
+	start_urls=[
+         'https://quotes.toscrape.com/'
+	]
+
+	def parse(self,response):
+
+		items = QbScrapeItem()
+
+		all_div_quotes = response.css('div.quote')
+
+		for quotes in all_div_quotes:
+			title = quotes.css('span.text::text').extract()
+			author = quotes.css('.author::text').extract()
+			tag = quotes.css('.tag::text').extract()
+
+			items['title'] = title
+			items['author'] = author
+			items['tag'] = tag
+
+
+			yield items
+			
+		next_page = response.css('li.next a::attr(href)').get()
+
+		if next_page is not None:
+			yield response.follow( next_page, callback= self.parse)
+        
+
